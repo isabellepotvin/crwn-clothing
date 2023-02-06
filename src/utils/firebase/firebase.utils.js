1a6544bed6a9f5cbbff1fe75f8a66340bcf79e79
Isabelle Potvin <ipotvin@rossvideo.com>
2023-02-06 11:08:20 -0500

    added comment

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -75,4 +75,4 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback); // can also pass an error callback, completed callback, etc.
